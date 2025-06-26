c7a25c80debd724e7ade913d6956180f99b1b74b
MarcosFacundoFlores <facuflores1310@gmail.com>
2025-06-26 09:12:30 -0300

    footer

--- src/components/footerSection.vue
+++ src/components/footerSection.vue
@@ -2,7 +2,7 @@
     <div class="text-center mt-12">
       <a href="https://marcosfacundoflores.github.io/portfolio-personal-codigo">
                   <p class="text-white/60 text-sm">
-            by MarcosFacundoFlores - 2025
+            by MarcosFacundoFlores - 2025 | ¿Buscas desarrollador? Charlemos oportunidades > Click Aquí <
           </p>
       </a>
 
